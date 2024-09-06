e58053c1276d1244f1c0d0e83c53a5e5d3d16f00
u7447030 <natashapegler@gmail.com>
2024-09-07 09:34:51 +1000

    Modifications to support condition testing

--- module/conditions/conditions.js
+++ module/conditions/conditions.js
@@ -26,126 +26,151 @@ export const conditions=[
         id: CONDITIONS.blind,
         name: "CONDITIONS.blind",
         icon: "systems/fathomlessgears/assets/icons/Blind.png",
+        statuses: ["blind"]
     },
     {
         id: CONDITIONS.burdened,
         name: "CONDITIONS.burdened",
         icon: "systems/fathomlessgears/assets/icons/Burdened.png",
+        statuses: ["burdened"]
     },
     {
         id: CONDITIONS.concealed,
         name: "CONDITIONS.concealed",
         icon: "systems/fathomlessgears/assets/icons/Concealed.png",
+        statuses: ["concealed"]
     },
     {
         id: CONDITIONS.dazed,
         name: "CONDITIONS.dazed",
         icon: "systems/fathomlessgears/assets/icons/Dazed.png",
+        statuses: ["dazed"]
     },
     {
         id: CONDITIONS.doomed,
         name: "CONDITIONS.doomed",
         icon: "systems/fathomlessgears/assets/icons/Doomed.png",
+        statuses: ["doomed"]
     },
     {
         id: CONDITIONS.drained,
         name: "CONDITIONS.drained",
         icon: "systems/fathomlessgears/assets/icons/Drained.png",
+        statuses: ["drained"]
     },
     {
         id: CONDITIONS.evasive,
         name: "CONDITIONS.evasive",
         icon: "systems/fathomlessgears/assets/icons/Evasive.png",
+        statuses: ["evasive"]
     },
     {
         id: CONDITIONS.fatigued,
         name: "CONDITIONS.fatigued",
         icon: "systems/fathomlessgears/assets/icons/Fatigued.png",
+        statuses: ["fatigued"]
     },
     {
         id: CONDITIONS.focused,
         name: "CONDITIONS.focused",
         icon: "systems/fathomlessgears/assets/icons/Focused.png",
+        statuses: ["focused"]
     },
     {
         id: CONDITIONS.hover,
         name: "CONDITIONS.hover",
         icon: "systems/fathomlessgears/assets/icons/Hover.png",
+        statuses: ["hover"]
     },
     {
         id: CONDITIONS.ironclad,
         name: "CONDITIONS.ironclad",
         icon: "systems/fathomlessgears/assets/icons/Ironclad.png",
+        statuses: ["ironclad"]
     },
     {
         id: CONDITIONS.jammed,
         name: "CONDITIONS.jammed",
         icon: "systems/fathomlessgears/assets/icons/Jammed.png",
+        statuses: ["jammed"]
     },
     {
         id: CONDITIONS.quickened,
         name: "CONDITIONS.quickened",
         icon: "systems/fathomlessgears/assets/icons/Quickened.png",
+        statuses: ["quickened"]
     },
     {
         id: CONDITIONS.restrained,
         name: "CONDITIONS.restrained",
         icon: "systems/fathomlessgears/assets/icons/Restrained.png",
+        statuses: ["restrained"]
     },
     {
         id: CONDITIONS.slowed,
         name: "CONDITIONS.slowed",
         icon: "systems/fathomlessgears/assets/icons/Snared.png",
+        statuses: ["slowed"]
     },
     {
         id: CONDITIONS.stalwart,
         name: "CONDITIONS.stalwart",
         icon: "systems/fathomlessgears/assets/icons/Stalwart.png",
+        statuses: ["stalwart"]
     },
     {
         id: CONDITIONS.tranq,
         name: "CONDITIONS.tranq",
         icon: "systems/fathomlessgears/assets/icons/Tranq.png",
+        statuses: ["tranq"]
     },
     {
         id: CONDITIONS.wired,
         name: "CONDITIONS.wired",
         icon: "systems/fathomlessgears/assets/icons/Wired.png",
+        statuses: ["wired"]
     },
     {
         id: "hook1",
         name: "CONDITIONS.hook1",
         icon: "systems/fathomlessgears/assets/icons/Blue Hook.png",
+        statuses: ["hook1"]
     },
     {
         id: "hook2",
         name: "CONDITIONS.hook2",
         icon: "systems/fathomlessgears/assets/icons/Green Hook.png",
+        statuses: ["hook2"]
     },
     {
         id: "hook3",
         name: "CONDITIONS.hook3",
         icon: "systems/fathomlessgears/assets/icons/Orange Hook.png",
+        statuses: ["hook3"]
     },
     {
         id: "hook4",
         name: "CONDITIONS.hook4",
         icon: "systems/fathomlessgears/assets/icons/Pink Hook.png",
+        statuses: ["hook4"]
     },
     {
         id: "hook5",
         name: "CONDITIONS.hook5",
         icon: "systems/fathomlessgears/assets/icons/Purple Hook.png",
+        statuses: ["hook5"]
     },
     {
         id: "hook6",
         name: "CONDITIONS.hook6",
         icon: "systems/fathomlessgears/assets/icons/Red Hook.png",
+        statuses: ["hook6"]
     },
     {
         id: "hook7",
         name: "CONDITIONS.hook7",
         icon: "systems/fathomlessgears/assets/icons/Yellow Hook.png",
+        statuses: ["hook7"]
     },
 ]
 
@@ -211,7 +236,9 @@ export const IMPLEMENTED_CONDITIONS={
 
 export function initialiseEffectHooks() {
     Hooks.on("createActiveEffect",(activeEffect,diffData,userId) => {
+        console.log("Creating effect!")
         applyEffect(activeEffect.parent, activeEffect);
+        console.log(activeEffect.parent);
     });
 
     Hooks.on("deleteActiveEffect",(activeEffect,diffData,userId) => {
@@ -227,10 +254,17 @@ export function initialiseEffectHooks() {
 }
 
 export function applyEffect(actor,effect) {
+    console.log("Applying effect!")
+    console.log(effect);
     const statusName=effect.statuses.values().next().value;
     const thisEffect=findImplementedCondition(statusName);
+    console.log(statusName)
     if(ATTRIBUTE_ONLY_CONDITIONS.includes(statusName)) {
+        console.log("Is attribute only");
         let effectCounter = foundry.utils.getProperty(effect, "flags.statuscounter.counter");
+        if(!effectCounter) {
+            effectCounter = new ActiveEffectCounter(1,effect.icon,effect);
+        }
         applyConditionModifier(actor,thisEffect,effectCounter.value);
     } else {
         switch(thisEffect){
