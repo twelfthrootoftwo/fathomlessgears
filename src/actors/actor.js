44f4d2e5846bfd6f82dfc6c582eedbdbc9ca4ebe
twelfthrootoftwo <natashapegler@gmail.com>
2025-03-31 20:36:43 +1100

    Temp counter value

--- src/actors/actor.js
+++ src/actors/actor.js
@@ -245,9 +245,15 @@ export class HLMActor extends Actor {
 						NUMBERED_CONDITIONS.includes(conditionName) &&
 						!activeEffect.flags.statuscounter
 					) {
-						await quickCreateCounter(activeEffect, false);
+						// await quickCreateCounter(activeEffect, false);
 						// await timeout(200);
 						// activeEffect = await fromUuid(activeEffect.uuid);
+						// Create a temp value (this will be overridden when the actual value gets saved)
+						activeEffect.flags.statuscounter = {
+							counter: {
+								value: 1
+							}
+						};
 					}
 					if (
 						Array.from(
